7475a86e559fbc4ae843cc4f80e11ddcbf1dd251
Ademir <ademir.m.junior@gmail.com>
2021-08-15 13:09:00 -0300

    Add first form validation

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'dio-cadastro-filmes',
@@ -15,10 +15,26 @@ export class CadastroFilmesComponent implements OnInit {
   ngOnInit() {
 
     this.cadastro = this.fb.group({
-      hideRequired: false,
-      floatLabel: 'auto',
+      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dtLancamento: ['', [Validators.required]],
+      descricao: [''],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDb: ['', [Validators.minLength(10)]],
+      genero: ['', [Validators.required]]
     });
 
+  }
+
+  salvar(): void {
+    if (this.cadastro.invalid) {
+      return;
+    }
+    alert('SUCESSO!!\n\n' + JSON.stringify(this.cadastro.value, null, 4));
+  }
+
+  reiniciarForm(): void {
+    this.cadastro.reset();
   }
 
 }

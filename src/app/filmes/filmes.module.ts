19a82a609e873f49b5ee7ad8d4dfd20cebcfbeb8
Ademir <ademir.m.junior@gmail.com>
2021-08-17 17:53:59 -0300

    Begin to make inputs as components

--- src/app/filmes/filmes.module.ts
+++ src/app/filmes/filmes.module.ts
@@ -5,12 +5,15 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CadastroFilmesComponent } from './cadastro-filmes/cadastro-filmes.component';
 import { MaterialModule } from '../shared/material/material.module';
 import { ListagemFilmesComponent } from './listagem-filmes/listagem-filmes.component';
+import { CamposModule } from '../shared/components/campos/campos.module';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    CamposModule
   ],
   declarations: [CadastroFilmesComponent, ListagemFilmesComponent]
 })

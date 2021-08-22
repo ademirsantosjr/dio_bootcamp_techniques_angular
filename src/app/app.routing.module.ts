5f5ed62bb03f973979b24e40da0cf7ccdfdebca9
Ademir <ademir.m.junior@gmail.com>
2021-08-22 10:53:19 -0300

    Develop route to 'edit'

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -20,12 +20,22 @@ const routes: Routes = [
         component: ListagemFilmesComponent
       },
       {
-        path: ':id',
-        component: VisualizarFilmesComponent
+        path: 'cadastro',
+        children: [
+          {
+            path: '',
+            component: CadastroFilmesComponent
+          },
+          {
+            path: ':id',
+            component: CadastroFilmesComponent,
+            pathMatch: 'full'
+          }
+        ]
       },
       {
-        path: 'cadastro',
-        component: CadastroFilmesComponent,
+        path: ':id',
+        component: VisualizarFilmesComponent,
         pathMatch: 'full'
       }
     ]
